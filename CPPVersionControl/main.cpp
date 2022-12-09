ce8b81d13e98eef0d932cfbced26bb6408c3828c
Neamtu Costin Cristian <caostineu@yahoo.com>
2022-12-09 14:24:47 +0200

    Added greet function.

--- CPPVersionControl/main.cpp
+++ CPPVersionControl/main.cpp
@@ -1,11 +1,14 @@
 # include <iostream>
 #include "feature.h"
 
-
-int main()
+void greet()
 {
 	//print some text
 	std::cout << "Main Function Welcome\n\n";
 
 	std::cout << "Main Function Hello World\n\n\n";
+}
+int main()
+{
+	greet();
 }
